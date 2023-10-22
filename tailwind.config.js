7a77fdf4d9dbc71e1707c668582276786da40efd
Michał Miotła <miotlamichal@gmail.com>
2023-10-22 15:49:39 +0200

    file structure and first works with mobile view

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html'],
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			colors: {
+				Pink: 'hsl(322, 100%, 66%)',
+				LightPink: 'hsl(321, 100%, 78%)',
+				LightRed: 'hsl(0, 100%, 63%)',
+				VeryDarkCyan: 'hsl(192, 100%, 9%)',
+				VeryPaleBlue: 'hsl(207, 100%, 98%)',
+			},
+			fontFamily: {
+				Poppins: ['Poppins', 'sans-serif'],
+				Sans: ['Open Sans', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+}
